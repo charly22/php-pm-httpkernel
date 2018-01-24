f0f2ad7ee71a178b7bed874d21d965257a814ee6
charly22 <carlos@olapic.com>
2018-01-24 12:15:14 -0300

    Simplify Silex Bootstrap

--- Bootstraps/Silex.php
+++ Bootstraps/Silex.php
@@ -5,48 +5,15 @@ namespace PHPPM\Bootstraps;
 use PHPPM\Bootstraps\BootstrapInterface;
 use Silex\Application;
 
-class Silex implements BootstrapInterface, ApplicationEnvironmentAwareInterface
+class Silex implements BootstrapInterface
 {
-    /**
-     * @var string
-     */
-    private $appEnv;
-
-    /**
-     * @var bool
-     */
-    private $debug;
-
-    /**
-     * Instantiate the bootstrap, storing the $appenv
-     *
-     * @param $appenv
-     * @param $debug
-     */
-    public function initialize($appenv, $debug)
-    {
-        $this->appenv = $appenv;
-        $this->debug = $debug;
-    }
 
     /**
      * @return Application
      */
     public function getApplication()
     {
-        require './vendor/autoload.php';
-
-        $app = new Application($this->appenv);
-
-        return $app;
-    }
-
-    /**
-     * @return string
-     */
-    public function getStaticDirectory()
-    {
-        return './web';
+        return require './app/bootstrap.php';
     }
 
 }
